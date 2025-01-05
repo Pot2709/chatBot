f247589838d6bf5ccbf2ded20ceedadfac652fa0
Pot2709 <jordan.potenciano@ismem.edu.pe>
2025-01-05 09:57:33 -0500

    primer commit

---
+++ sett.py
@@ -0,0 +1,29 @@
+token = 'alfa'
+
+whatsapp_token ='EAAGnPc7uYEMBO57See1QzITyEFEs8Q8pZBNVLb3XqH37YGVgEAYOKRBf8OyoUyxllZC2fkwdvDiOh1OUofQWVUiY95G8Pnvy5VRqOZAixoptCjva8Du8n8xIfqXIu4rvN5c9M6tHCVNpZBzVLCqkZAxS2DAMpwZC3YVFdGQQZC6LKbey14Y5q7vCy060AFSr92cSdqz8ZBlOP7b8D3bogoSSpZBSmeIMZD'
+
+whatsapp_url = 'https://graph.facebook.com/v21.0/549058584950227/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
+
+
+
+
+
+
