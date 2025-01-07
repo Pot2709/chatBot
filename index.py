b3c18adbcb41777259b4deb985e7bc1396894bbe
Pot2709 <jordan.potenciano@ismem.edu.pe>
2025-01-07 18:47:54 -0500

    errere

--- index.py
+++ index.py
@@ -1,35 +1,35 @@
 from flask import Flask, request
-import sett
+import sett 
 import services
 
 app = Flask(__name__)
 
 @app.route('/bienvenido', methods=['GET'])
-def bienvenido():
-    return 'Hola Team Alfa, desde flask'
+def  bienvenido():
+    return 'Hola mundo bigdateros, desde Flask'
 
 @app.route('/webhook', methods=['GET'])
 def verificar_token():
-    try: 
+    try:
         token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
 
-        if token == sett.token and challenge is not None:
+        if token == sett.token and challenge != None:
             return challenge
         else:
             return 'token incorrecto', 403
     except Exception as e:
-        return str(e), 403
-
+        return e,403
+    
 @app.route('/webhook', methods=['POST'])
 def recibir_mensajes():
-    try: 
+    try:
         body = request.get_json()
         entry = body['entry'][0]
         changes = entry['changes'][0]
         value = changes['value']
         message = value['messages'][0]
-        number = message['from']
+        number = services.replace_start(message['from'])
         messageId = message['id']
         contacts = value['contacts'][0]
         name = contacts['profile']['name']
@@ -37,9 +37,9 @@ def recibir_mensajes():
 
         services.administrar_chatbot(text, number,messageId,name)
         return 'enviado'
-    
+
     except Exception as e:
-        return 'No enviado ' + str(e), 400
+        return 'no enviado ' + str(e)
 
 if __name__ == '__main__':
     app.run()
